81cf358eb0742eae7c8824e5ec94cd8c070c5e4e
teemal <taymal1987@gmail.com>
2023-02-25 10:22:17 -0700

    add remove BP by regex

--- src/extension.ts
+++ src/extension.ts
@@ -2,25 +2,107 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+const handleInput = async () => {
+	const input = vscode.window.showInputBox()
+		.then((out) => {
+			if (out) {
+				return out.trim();
+			}
+		});
+	return input;
+};
+
+const getChunkedRowBounds = (row: number, columnStart: number, chunkSize: number) => {
+	const lineStart = new vscode.Position(row, columnStart);
+	const lineEnd = new vscode.Position(row + chunkSize, columnStart);
+	return [lineStart, lineEnd];
+};
+
+const getTextChunk = (start: vscode.Position, stop: vscode.Position) => {
+	const range = new vscode.Range(start, stop);
+	return vscode.window.activeTextEditor?.document.getText(range);
+};
+
+const selectBreakpointsChunked = (pattern: string | undefined) => {
+	if (!pattern) { return; }
+	let editor = vscode.window.activeTextEditor;
+	if (!editor) {
+		vscode.window.showInformationMessage('Open a file first to manipulate text selections');
+		return;
+	}
+	let document = editor.document;
+	const documentLength = vscode.window.activeTextEditor?.document.lineCount;
+	if (!documentLength) { return; }
+	const columnStart = 0;
+	const chunkSize = 100;
+	const bps: vscode.Breakpoint[] = [];
+	for (let i = 0; i < documentLength; i += chunkSize) {
+		let [lineStart, lineEnd] = getChunkedRowBounds(i, columnStart, chunkSize);
+		const textChunk = getTextChunk(lineStart, lineEnd);
+		if (!textChunk) { return; }
+		let lines = textChunk.split('\n');
+		for (let line = 0; line < lines.length; line++) {
+			const lineMatch = lines[line].match(pattern);
+			if (!lineMatch) { continue; }
+			const editorLine = i + line;
+			bps.push(new vscode.SourceBreakpoint(new vscode.Location(document.uri, new vscode.Position(editorLine, 0))));
+		}
+		return bps;
+	}
+};
+
+const findCurrentBreakPoints = (pattern: string | undefined) => {
+	if (!pattern) { return; }
+	let editor = vscode.window.activeTextEditor;
+	if (!editor) {
+		vscode.window.showInformationMessage('Open a file first to manipulate text selections');
+		return;
+	}
+	let document = editor.document;
+	let selection = editor.selections[0];
+
+	const found = vscode.debug.breakpoints.filter(bp => {
+		if (bp instanceof vscode.SourceBreakpoint) {
+			const bpline = bp.location.range.start.line;
+			const {text} = document.lineAt(bpline);
+			const matcher = text.match(pattern) !== null;
+			return matcher;
+		}
+		return false;
+	});
+	return found;
+};
+
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "yeetpoint" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let regex = vscode.commands.registerCommand('yeetpoint.addRegex', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('CHUMBA WUMBA!!');
+	let addRegexBreakPoints = vscode.commands.registerCommand('yeetpoint.addRegex', () => {
+		handleInput()
+			.then((pattern) => {
+				return selectBreakpointsChunked(pattern);
+			})
+			.then((breakPoints) => {
+				if (breakPoints) {
+					vscode.debug.addBreakpoints(breakPoints);
+				}
+			});
 	});
+	context.subscriptions.push(addRegexBreakPoints);
 
-	context.subscriptions.push(regex);
+	let removeRegexBreakPoints = vscode.commands.registerCommand('yeetpoint.removeRegex', () => {
+		handleInput()
+			.then((pattern) => {
+				return findCurrentBreakPoints(pattern);
+			})
+			.then((breakPoints) => {
+				if (breakPoints) {
+					vscode.debug.removeBreakpoints(breakPoints);
+				}
+			});
+	});
+	context.subscriptions.push(removeRegexBreakPoints);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
